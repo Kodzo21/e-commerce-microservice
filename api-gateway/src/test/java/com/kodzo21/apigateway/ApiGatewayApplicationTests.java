ec7060b6f69f4b28efcc4293b56ff5273e43bea0
Lukasz <ryczekl21@gmail.com>
2023-09-20 23:52:42 +0200

    api gateway init

---
+++ api-gateway/src/test/java/com/kodzo21/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodzo21.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
