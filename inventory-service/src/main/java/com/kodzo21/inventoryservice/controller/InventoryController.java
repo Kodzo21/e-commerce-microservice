c6aa114b11c40ccd9581ad51b4463db70c9a233c
Lukasz <ryczekl21@gmail.com>
2023-09-20 23:27:36 +0200

    correction

--- inventory-service/src/main/java/com/kodzo21/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/kodzo21/inventoryservice/controller/InventoryController.java
@@ -3,9 +3,11 @@ package com.kodzo21.inventoryservice.controller;
 
 import com.kodzo21.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1/inventory")
+@RestController
+@RequestMapping("api/v1/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
 
--- product-service/src/main/java/com/kodzo21/controller/ProductController.java
+++ product-service/src/main/java/com/kodzo21/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.kodzo21.controller;
 
 import com.kodzo21.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/")
+@RestController
+@RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
