f129a84bdb8898faa502f603ed94b6ce44eafe55
Lukasz <ryczekl21@gmail.com>
2023-09-26 23:04:18 +0200

    added inventory service methods

--- inventory-service/src/main/java/com/kodzo21/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/kodzo21/inventoryservice/repository/InventoryRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface InventoryRepository extends MongoRepository<Inventory, String> {
 
+    Inventory findBySKU(String sku);
 }
 
--- inventory-service/src/main/java/com/kodzo21/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/kodzo21/inventoryservice/service/InventoryService.java
@@ -1,5 +1,6 @@
 package com.kodzo21.inventoryservice.service;
 
+import com.kodzo21.inventoryservice.model.Inventory;
 import com.kodzo21.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,37 @@ public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
+
+    public Integer getQuantity(String SKU) {
+        return inventoryRepository.findBySKU(SKU).getQuantity();
+    }
+
+    public void updateQuantity(String SKU, Integer quantity) {
+        inventoryRepository.findBySKU(SKU).setQuantity(quantity);
+    }
+
+    public void addQuantity(String SKU, Integer quantity) {
+        inventoryRepository.findBySKU(SKU).setQuantity(inventoryRepository.findBySKU(SKU).getQuantity() + quantity);
+    }
+
+    public void subtractQuantity(String SKU, Integer quantity) {
+        inventoryRepository.findBySKU(SKU).setQuantity(inventoryRepository.findBySKU(SKU).getQuantity() - quantity);
+    }
+
+    public void deleteInventory(String SKU) {
+        inventoryRepository.delete(inventoryRepository.findBySKU(SKU));
+    }
+
+    public void createInventory(String SKU, Integer quantity) {
+        inventoryRepository.save(
+                Inventory.builder()
+                        .SKU(SKU)
+                        .quantity(quantity)
+                        .build()
+        );
+    }
+
+    public Boolean checkIfIsInStock(String SKU) {
+        return inventoryRepository.findBySKU(SKU).getQuantity() > 0;
+    }
 }
